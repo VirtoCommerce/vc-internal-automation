7543020d415ae957e7199e9455cd272701badd30
Leo <leon@entechsolutions.com>
2020-05-26 08:58:44 +0400

    Add other repositories to update

--- platform-v3-tools/UpdateBranchSettings.ps1
+++ platform-v3-tools/UpdateBranchSettings.ps1
@@ -6,13 +6,45 @@ param(
 $Branches = "dev", "master"
 
 if (-not (Get-Module -ListAvailable -Name PowerShellForGitHub)) {
-    Install-Module PowerShellForGitHub
+    Install-Module PowerShellForGitHub -Force
 }
 
 Import-Module PowerShellForGitHub
 
 $repositories =
-    "vc-platform"
+    "vc-platform",
+    "vc-storefront-core",
+    "vc-module-Authorize.Net",
+    "vc-module-avatax",
+    "vc-module-azure-search",
+    "vc-module-bulk-actions",
+    "vc-module-cart",
+    "vc-module-catalog",
+    "vc-module-catalog-csv-import",
+    "vc-module-catalog-personalization",
+    "vc-module-catalog-publishing",
+    "vc-module-content",
+    "vc-module-core",
+    "vc-module-customer",
+    "vc-module-customer-review",
+    "vc-module-elastic-search",
+    "vc-module-export",
+    "vc-module-image-tools",
+    "vc-module-inventory",
+    "vc-module-lucene-search",
+    "vc-module-marketing",
+    "vc-module-notification",
+    "vc-module-order",
+    "vc-module-pagebuilder",
+    "vc-module-payment",
+    "vc-module-pricing",
+    "vc-module-quote",
+    "vc-module-search",
+    "vc-module-shipping",
+    "vc-module-sitemaps",
+    "vc-module-store",
+    "vc-module-subscription",
+    "vc-module-tax"
 
 $template = Get-Content "rep-branch-settings.json" | Out-String
 
@@ -30,7 +62,7 @@ $repositories | ForEach-Object {
             'Method' = 'PUT'
             'AccessToken' = $AccessToken
             'UriFragment' = "repos/$($Organization)/$($Repository)/branches/$($Branch)/protection"
-            'Description' =  "Get master branch protection settings for $($Repository)"
+            'Description' =  "Set $($Repository) protection settings for $($Repository)"
         }
         
         Invoke-GHRestMethod @params -Body $settings | Out-Null
