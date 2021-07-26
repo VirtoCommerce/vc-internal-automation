f64fdcc691e2545694b2d54aaf0503d0058fa523
Maksim <mko@virtoway.com>
2021-07-26 18:35:09 +0700

    PT-2272: Add pares function

--- create-regression-sprint-report/RegressionSprintReport/index.ts
+++ create-regression-sprint-report/RegressionSprintReport/index.ts
@@ -1,6 +1,38 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import fetch from "node-fetch";
+import * as yaml from "js-yaml";
+
+
+interface ModuleInfo
+{
+    id: string,
+    version: string
+}
+
+async function parseModulesVersion(modulesList: any[]){
+    let modulesVersionArray: ModuleInfo[] = [];
+    for( let module of modulesList){
+        const regexVersion = /_\d+\.\d+\.\d+/;
+        let packageUrl: string = module["PackageUrl"];
+        let moduleVersion: ModuleInfo = {
+            id: module["Id"].replace("VirtoCommerce.","") ,
+            version: module["Id"] + packageUrl.match(regexVersion)?.[0]
+        }
+        modulesVersionArray.push(moduleVersion);
+    }
+    return modulesVersionArray;
+}
+
+async function getModulesList(configMapUrl: string){
+    const response = await fetch(configMapUrl);
+    const doc = yaml.load(await response.text());
+    let modulesList = doc["data"]?.["modules.json"];
+    return await parseModulesVersion(JSON.parse(modulesList));
+}
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const configMapUrl = "https://raw.githubusercontent.com/VirtoCommerce/vc-deploy-apps/qa/regression-app/overlays/qa/deployment-cm.yaml";
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
@@ -12,8 +44,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         body: responseMessage
     };
 
-    let response = await fetch("https://github.com/VirtoCommerce/vc-deploy-apps/blob/qa/regression-app/overlays/qa/deployment-cm.yaml");
-    let deploymentCm = await response.blob();
+    let deploymentCm = await getModulesList(configMapUrl);
     console.log(deploymentCm);
 };
 
