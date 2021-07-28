19f6913b8000dead11557cf1d86c51ce92ea4ff6
Maksim <mko@virtoway.com>
2021-07-28 07:52:31 +0700

    PT-2272: Add createPageContent function

--- create-regression-sprint-report/RegressionSprintReport/index.ts
+++ create-regression-sprint-report/RegressionSprintReport/index.ts
@@ -9,6 +9,26 @@ interface ModuleInfo
     version: string
 }
 
+interface ConfluencePageInfo
+{
+    type: string,
+    title: string,
+    ancestors:[
+        {
+            id:number
+        }
+    ], 
+    space:{
+        key: string
+    },
+    body:{
+        storage:{
+            value: string,
+            representation: string
+        }
+    }
+}
+
 async function parseModulesVersion(modulesList: any[]){
     let modulesVersionArray: ModuleInfo[] = [];
     for( let module of modulesList){
@@ -30,22 +50,65 @@ async function getModulesList(configMapUrl: string){
     return await parseModulesVersion(JSON.parse(modulesList));
 }
 
+async function createConfluencePage(confluencePageSettings: ConfluencePageInfo, ){
+}
+
+
+function createPageContent(modulesVersion: ModuleInfo[], currentSprint: string){
+    let pageBody: string = '<table>';
+    let rowN: number = 0;
+    pageBody += '<tr> <th>#</th> <th>Module Name</th> <th>Module Version</th> <th>Existed integrations</th> <th>Type testing</th> <th>Issues from previous sprints</th> <th>Issues</th> </tr>'
+    for(let module of modulesVersion){
+        pageBody += `<tr> <td>${rowN ++}</td> <td>${module.id}</td> <td>${module.version}</td> <td></td> <td></td> <td></td> <td></td> </tr>`
+    }
+    pageBody += '</table>';
+
+    return pageBody;
+}
+
+function createPageSettings(pageType: string, pageTitle: string, spaceKey: string, subpageId: number, pageBody: string, pageRepresentation: string): ConfluencePageInfo{
+    const pageSettings: ConfluencePageInfo = 
+    {
+        type: pageType,
+        title: pageTitle,
+        ancestors:[
+            {
+                id: subpageId
+            }
+        ], 
+        space:{
+            key: spaceKey
+        },
+        body:{
+            storage:{
+                value: pageBody,
+                representation: pageRepresentation
+            }
+        }
+    };
+    return pageSettings;
+}
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const configMapUrl = "https://raw.githubusercontent.com/VirtoCommerce/vc-deploy-apps/qa/regression-app/overlays/qa/deployment-cm.yaml";
+//    const configMapUrl = "https://raw.githubusercontent.com/VirtoCommerce/vc-deploy-apps/qa/regression-app/overlays/qa/deployment-cm.yaml";
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
+
+    const configMapUrl = req.body && req.body.configMapUrl;
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : `Body: ${req.body}`;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 
-    let deploymentCm = await getModulesList(configMapUrl);
+    const currentSprint = "7";
+    const deploymentCm = configMapUrl ? await getModulesList(configMapUrl) : null;
+    const pageBody = createPageContent(deploymentCm, currentSprint)
     console.log(deploymentCm);
+    console.log(pageBody);
 };
 
 export default httpTrigger;
