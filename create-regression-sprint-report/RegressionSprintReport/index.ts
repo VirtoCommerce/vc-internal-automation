844cafbd1f9038261173dfc95717e79d15ef39ba
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2021-08-05 18:03:27 +0700

    PT-2272: Generate confluence document after close regression sprint (#2)
    * PT-2272: Add function body
    
    * PT-2272: Add pares function
    
    * PT-2272: Add createPageContent function
    
    * PT-2272: Add publishPageContent function
    
    * PT-2272: Add links to the module issues
    
    * PT-2272: Add issues card
    
    * Add Issues card macro
    * Add platform version
    * Add publishing results handle
    
    * Update create-regression-sprint-report/RegressionSprintReport/index.ts
    
    Co-authored-by: Filipp Kuksov <47894313+Vectorfield4@users.noreply.github.com>
    
    * PT-2272: Add guard class
    
    * Add Issues card macro
    * Add platform version
    * Add publishing results handle
    
    * PT-2272: Add checkPageNotExist function
    
    * PT-2272: Add  bicep file
    
    * PT-2272: Add deployment approval
    
    * PT-2272: Add deployment approval
    
    * PT-2272: Add deployment approval
    
    * PT-2272: Add deployment approval
    
    * PT-2272: Add deployment approval
    
    * PT-2272: Add deployment approval
    
    * Shell command
    
    * PT-2272: Fix bicep input parameters
    
    * Shell command
    
    * PT-2272: Add parametere to deploy
    
    * Shell command
    
    * PT-2272: Fix parameters
    
    * Shell bash
    
    * PT-2272: Fix parameters
    
    * Shell bash
    
    * PT-2272: Fix parameters
    
    * PT-2272: Fix workflow
    
    * PT-2272: Fix parameters
    
    * PT-2272: Fix parameters
    
    * PT-2272: Fix deployment
    
    * PT-2272: Fix deployment
    
    * PT-2272: Fix deployment
    
    * PT-2272: Fix deployment
    
    * PT-2272: Fix deployment
    
    * PT-2272: Add appInsights
    
    * PT-2272: Fix appInsights
    
    * PT-2272: Fix naming
    
    * PT-2272: Fix appinsights
    
    * PT-2272: Add link to parent issue
    
    * PT-2272: Fix typo
    
    * PT-2272: Deploy only from master branch
    
    Co-authored-by: Filipp Kuksov <47894313+Vectorfield4@users.noreply.github.com>
    Co-authored-by: Konstantin Kisilevskiy <xlonelysoul94@gmail.com>

---
+++ create-regression-sprint-report/RegressionSprintReport/index.ts
@@ -0,0 +1,340 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import fetch from "node-fetch";
+import * as yaml from "js-yaml";
+
+const loginSecretParam = "ConfluenceLogin";
+const passwordSecretParam = "ConfluenceToken";
+const confluenceUrlParam = "ConfluenceUrl";
+const jiraUrlParam = "jiraUrl";
+const modulePrefix = "VirtoCommerce.";
+const platformKey = "docker.pkg.github.com/virtocommerce/vc-platform/platform";
+
+
+interface ModuleInfo
+{
+    id: string,
+    version: string,
+    sprintIssues: string,
+    oldIssues: string
+}
+
+interface ConfluencePageInfo
+{
+    type: string,
+    title: string,
+    ancestors:[
+        {
+            id:number
+        }
+    ], 
+    space:{
+        key: string
+    },
+    body:{
+        storage:{
+            value: string,
+            representation: string
+        }
+    }
+}
+
+function getHostName(url: string){
+    const regexUrl = /^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i; // will return domain name without protocol and path %www.github.com%
+    const matches = url.match(regexUrl);
+    // extract hostname (will be null if no match is found)
+    return matches && matches[1];
+}
+
+
+class Guard
+{
+    public static AgainstNull(argument: any, argumentName: string): any
+    {
+        if (argument == null)
+        {
+            throw new Error(`ArgumentNullException: ${argumentName} value cannot be null`) ;
+        }
+    }
+}
+
+async function getPlatformVersion(kustomizationUrl:string){
+
+    console.log("Get Platform version");
+
+    const response = await fetch(kustomizationUrl);
+    const doc = yaml.load(await response.text());
+    const imageIndex = doc["images"].findIndex( x => x.name === platformKey );
+
+    let platformVersion: ModuleInfo = {
+        id:  "Platform",
+        version: doc["images"][imageIndex]["newTag"],
+        oldIssues: null,
+        sprintIssues: null
+    }
+
+    Guard.AgainstNull(platformVersion, 'platformVersion');
+
+    return platformVersion;
+}
+
+function parseModulesVersion(modulesList: any[]){
+
+    let modulesVersionArray: ModuleInfo[] = [];
+
+    for( let module of modulesList){
+        const regexVersion = /_\d+\.\d+\.\d+/;
+        let packageUrl: string = module["PackageUrl"];
+        let moduleVersion: ModuleInfo = {
+            id: module["Id"].replace(modulePrefix ,"") ,
+            version: module["Id"] + packageUrl.match(regexVersion)?.[0],
+            oldIssues: null,
+            sprintIssues: null
+        }
+        modulesVersionArray.push(moduleVersion);
+    }
+    return modulesVersionArray;
+}
+
+async function getModulesList(configMapUrl: string){
+
+    console.log("Get modules list");
+
+    const response = await fetch(configMapUrl);
+    const doc = yaml.load(await response.text());
+    let modulesList = doc["data"]?.["modules.json"];
+    
+    const result = parseModulesVersion(JSON.parse(modulesList));
+    
+    Guard.AgainstNull(result, 'result');
+    
+    return result;
+}
+
+async function getModuleIssues(moduleName: string, parentIssueKey:string, inParent: boolean, issueType: string, jiraUrl: string, userName: string, password: string){
+
+    if (inParent) {
+        console.log(`Search ${moduleName} module issues for current regression sprint`);
+    } else {
+        console.log(`Search ${moduleName} module issues for previous sprints`);
+    }
+
+    let issuesLinks:string = "";
+
+    const headers = { 
+        'Authorization': `Basic ` + Buffer.from(`${userName}:${password}`).toString('base64'),
+        'Accept': 'application/json'
+    }
+    const operator: string = inParent ? "=" : "!="
+    const query: string = `/search?jql=parent ${operator} ${parentIssueKey} AND issuetype = ${issueType} AND "Component[Dropdown]" = ${moduleName} AND labels IN (Regression) AND labels NOT IN (FixedOnRegression)`;
+    const url = jiraUrl.replace(/\/+$/, '') + query; // remove trailing slashes
+    
+    try {
+        
+        const response = await fetch(
+            url, {
+            method: 'GET',
+            headers: headers
+            }
+        );
+
+        const result = JSON.parse(await response.text());
+        if (result["issues"]) {
+            const issuesArray = result["issues"];
+            for (let index = 0; index < issuesArray.length; index++) {
+                issuesLinks += `<p><ac:structured-macro ac:name=\"jira\" ac:schema-version=\"1\" ac:macro-id=\"e6f873c8-59d1-4ae2-82ff-bf8e2378369f\"><ac:parameter ac:name=\"server\">System JIRA</ac:parameter><ac:parameter ac:name=\"serverId\">ddb34fca-5878-3e2d-898b-cb89d86c7acf</ac:parameter><ac:parameter ac:name=\"key\">${issuesArray[index]["key"]}</ac:parameter></ac:structured-macro></p>`
+            }
+        }
+    } catch (error) {
+        console.error(error)
+    }
+
+    return issuesLinks;
+}
+
+async function fillModulesIssues(modulesList: ModuleInfo[],parentIssueKey: string, issueType: string, jiraUrl: string,  userName: string, password: string) {
+
+    console.log(`Start search issues`);
+
+    Guard.AgainstNull(modulesList, 'modulesList');
+    
+    for (let index = 0; index < modulesList.length; index++) {
+        const sprintIssues: string = await getModuleIssues(modulesList[index].id, parentIssueKey, true, issueType, jiraUrl, userName, password) ;
+        const oldIssues: string = await getModuleIssues(modulesList[index].id, parentIssueKey, false, issueType, jiraUrl, userName, password) ;
+        modulesList[index].sprintIssues = sprintIssues;
+        modulesList[index].oldIssues = oldIssues;
+    }
+    
+    Guard.AgainstNull(modulesList, 'modulesList');
+
+    return modulesList;
+}
+
+
+async function publishConfluencePage(confluencePageSettings: ConfluencePageInfo, confluenceUrl: string, userName: string, password: string){
+
+    console.log("Publishing Confluence page");
+
+    const body: string = JSON.stringify(confluencePageSettings);
+    const headers = { 
+        'Authorization': `Basic ` + Buffer.from(`${userName}:${password}`).toString('base64'),
+        'Content-Type': 'application/json'
+    }
+    try {
+        const response = await fetch(confluenceUrl, {
+            method: 'POST',
+            body: body,
+            headers: headers
+        });
+
+        return response;
+    
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function createPageContent(modulesList: ModuleInfo[], parentIssueKey: string){
+
+    console.log("Creating Confluence page content");
+
+    Guard.AgainstNull(modulesList, 'loginSecretParam');
+
+    let pageBody: string = `<p><ac:structured-macro ac:name=\"jira\" ac:schema-version=\"1\" ac:macro-id=\"e6f873c8-59d1-4ae2-82ff-bf8e2378369f\"><ac:parameter ac:name=\"server\">System JIRA</ac:parameter><ac:parameter ac:name=\"serverId\">ddb34fca-5878-3e2d-898b-cb89d86c7acf</ac:parameter><ac:parameter ac:name=\"key\">${parentIssueKey}</ac:parameter></ac:structured-macro></p>`;
+    let rowN: number = 0;
+
+    pageBody += '<p></p>'
+    pageBody += '<table><tbody>'
+    pageBody += '<tr> <th>#</th> <th>Module Name</th> <th>Module Version</th> <th>Type testing</th> <th>Issues from previous sprints</th> <th>Issues</th> </tr>'
+
+    for(let module of modulesList){
+        pageBody += `<tr> <td>${++ rowN}</td> <td>${module.id}</td> <td>${module.version}</td> <td></td> <td>${module.oldIssues}</td> <td>${module.sprintIssues}</td> </tr>`
+    }
+    pageBody += '</tbody></table>';
+
+    Guard.AgainstNull(pageBody, 'pageBody');
+
+    return pageBody;
+}
+
+function createPageSettings(pageType: string, pageTitle: string, spaceKey: string, subpageId: number, pageBody: string, pageRepresentation: string): ConfluencePageInfo {
+    const pageSettings: ConfluencePageInfo = {
+        type: pageType,
+        title: pageTitle,
+        ancestors: [{
+            id: subpageId
+        }], 
+        space: {
+            key: spaceKey
+        },
+        body: {
+            storage: {
+                value: pageBody,
+                representation: pageRepresentation
+            }
+        }
+    };
+
+    Guard.AgainstNull(pageSettings, 'pageSettings');
+
+    return pageSettings;
+}
+
+async function checkPageNotExist(pageTitle:string, spaceKey: string, confluenceUrl: string, userName: string, password: string) {
+    console.log("Check Confluence page exists");
+
+    const queryUrl: string = confluenceUrl + `\?title=${pageTitle}&spaceKey=${spaceKey}&status=current`;
+    const headers = { 
+        'Authorization': `Basic ` + Buffer.from(`${userName}:${password}`).toString('base64'),
+    }
+    try {
+        const response = await fetch(queryUrl, {
+            method: 'GET',
+            headers: headers
+        });
+
+        let resJson = await response.json();
+        let result = resJson["size"] > 0 ? false: true;
+        return result;
+    
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    context.log('HTTP trigger function processed a request.');
+
+    let responseMessage ;
+    let status= 200;
+
+    try {
+        
+        const login = process.env[loginSecretParam];
+        Guard.AgainstNull(login, loginSecretParam);
+        const password = process.env[passwordSecretParam];
+        Guard.AgainstNull(password, passwordSecretParam);
+        const confluenceUrl = process.env[confluenceUrlParam];
+        Guard.AgainstNull(confluenceUrl, confluenceUrlParam);
+        const jiraUrl = process.env[jiraUrlParam];
+        Guard.AgainstNull(jiraUrl, jiraUrlParam);
+
+        const configMapUrl = req.body && req.body.configMapUrl;
+        Guard.AgainstNull(configMapUrl, 'configMapUrl');
+        const kustomizationUrl = req.body && req.body.kustomizationUrl;
+        Guard.AgainstNull(kustomizationUrl, 'kustomizationUrl');
+        const pageType = req.body && req.body.pageType; 
+        Guard.AgainstNull(pageType, 'pageType');
+        const pageTitle = req.body && req.body.pageTitle; 
+        Guard.AgainstNull(pageTitle, 'pageTitle');
+        const spaceKey = req.body && req.body.spaceKey; 
+        Guard.AgainstNull(spaceKey, 'spaceKey');
+        const subpageId = req.body && req.body.subpageId; 
+        Guard.AgainstNull(subpageId, 'subpageId');
+        const pageRepresentation = req.body && req.body.pageRepresentation;
+        Guard.AgainstNull(pageRepresentation, 'pageRepresentation');
+
+        const issueType = req.body && req.body.issueType;
+        Guard.AgainstNull(issueType, 'issueType');
+        const parentIssueKey = req.body && req.body.parentIssueKey;
+        Guard.AgainstNull(parentIssueKey, 'parentIssueKey');
+
+        if (await checkPageNotExist(pageTitle, spaceKey, confluenceUrl, login, password)) {
+
+            let modulesList: any[] = await getModulesList(configMapUrl);
+            modulesList = modulesList.sort((a,b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
+            modulesList.unshift(await getPlatformVersion(kustomizationUrl));
+            
+            modulesList = await fillModulesIssues(modulesList, parentIssueKey, issueType, jiraUrl, login, password);
+    
+            const pageBody = createPageContent(modulesList, parentIssueKey);
+            const pageSettings = createPageSettings(pageType, pageTitle,spaceKey, subpageId, pageBody,pageRepresentation);
+            const publishResult = await publishConfluencePage(pageSettings, confluenceUrl, login, password);
+    
+            if (publishResult && publishResult.status === 200) {
+                responseMessage = "Regression sprint report executed successfully."
+            } else {
+                responseMessage = publishResult ? await publishResult.json() : "Regression sprint report publishing error";
+                status =  publishResult ?  publishResult.status: 400;
+            }
+        } else {
+            responseMessage = `Page with "${pageTitle}" title already exists.`;
+            status = 400;
+        }
+
+    } catch (error) {
+        console.error(error);
+        status = 400;
+        responseMessage = error;
+    }
+
+    console.log(responseMessage);
+
+    context.res = {
+        status: status, 
+        body: responseMessage
+    };
+};
+
+export default httpTrigger;
---
+++ create-regression-sprint-report/main.bicep
@@ -0,0 +1,117 @@
+param functionAppName string = 'create-regression-report'
+param storageAccountName string  = 'fnstor${toLower(substring(replace(functionAppName, '-', ''), 0, 17))}'
+param appServicePlanName string = 'fn-${functionAppName}-ServicePlan'
+param appInsightsName string = 'fn-${functionAppName}-AppInsights'
+param location string = resourceGroup().location
+
+@secure()
+param ConfluenceLogin string 
+@secure()
+param ConfluenceToken string
+
+param ConfluenceUrl string = 'https://virtocommerce.atlassian.net/wiki/rest/api/content/'
+param JiraUrl string = 'https://virtocommerce.atlassian.net/rest/api/3/'
+
+
+resource appInsights 'Microsoft.Insights/components@2020-02-02' = {
+  name: appInsightsName
+  location: location
+  kind: 'web'
+  properties: {
+    Application_Type: 'web'
+    IngestionMode: 'ApplicationInsights'
+    publicNetworkAccessForIngestion: 'Enabled'
+    publicNetworkAccessForQuery: 'Enabled'
+  }
+}
+
+resource storageAccount 'Microsoft.Storage/storageAccounts@2021-04-01' = {
+  name: storageAccountName
+  location: resourceGroup().location
+  kind: 'Storage'
+  sku: {
+    name: 'Standard_LRS'
+  }
+  properties: {
+    allowBlobPublicAccess: false
+  }
+}
+
+resource appServicePlan 'Microsoft.Web/serverfarms@2020-12-01' = {
+  name: appServicePlanName
+  location: location
+  sku: {
+    name: 'Y1'
+    tier: 'Dynamic'
+  }
+}
+
+resource functionApp 'Microsoft.Web/sites@2021-01-15' = {
+  name: functionAppName
+  location: location
+  kind: 'functionapp'
+  dependsOn: [
+    appServicePlan
+    storageAccount
+    appInsights
+  ]
+  properties: {
+    serverFarmId: appServicePlan.id
+    siteConfig: {
+      appSettings: [
+        
+        {
+          name: 'AzureWebJobsStorage'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${storageAccount.name};EndpointSuffix=${environment().suffixes.storage};AccountKey=${listKeys(storageAccount.id, storageAccount.apiVersion).keys[0].value}'
+        }
+        {
+          name: 'FUNCTIONS_EXTENSION_VERSION'
+          value: '~3'
+        }
+        {
+          name: 'FUNCTIONS_WORKER_RUNTIME'
+          value: 'node'
+        }
+        {
+          name: 'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING'
+          value: 'DefaultEndpointsProtocol=https;AccountName=${storageAccount.name};EndpointSuffix=${environment().suffixes.storage};AccountKey=${listKeys(storageAccount.id, storageAccount.apiVersion).keys[0].value}'
+        }
+        {
+          name: 'APPINSIGHTS_INSTRUMENTATIONKEY'
+          value: appInsights.properties.InstrumentationKey
+        }
+        {
+          name: 'APPLICATIONINSIGHTS_CONNECTION_STRING'
+          value: 'InstrumentationKey=${appInsights.properties.InstrumentationKey}'
+        }
+        {
+          name: 'WEBSITE_NODE_DEFAULT_VERSION'
+          value: '~14'
+        }
+        {
+          name: 'ConfluenceLogin'
+          value:ConfluenceLogin
+        }
+        {
+          name: 'ConfluenceToken'
+          value: ConfluenceToken
+        }
+        {
+          name: 'ConfluenceUrl'
+          value: ConfluenceUrl
+        }
+        {
+          name: 'JiraUrl'
+          value: JiraUrl
+        }
+        // WEBSITE_CONTENTSHARE will also be auto-generated - https://docs.microsoft.com/en-us/azure/azure-functions/functions-app-settings#website_contentshare
+        // WEBSITE_RUN_FROM_PACKAGE will be set to 1 by func azure functionapp publish
+        // WEBSITE_RUN_FROM_PACKAGE use to deploy azure function from zip package. Value shoul be public access url with zip package for download
+        // {
+        //   name: 'WEBSITE_RUN_FROM_PACKAGE'
+        //   value: 'https://github.com/mjisaak/azure-func-with-bicep/releases/download/v0.0.1/function.zip'
+        // }
+      ]
+    }
+  }
+}
